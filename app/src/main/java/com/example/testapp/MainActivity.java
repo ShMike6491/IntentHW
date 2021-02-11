bc4fc86c0bc85b7214501ec6d98bb1e1ad874a86
Mike Shilovski <ShMike6491@gmail.com>
2021-02-11 16:56:50 +0300

    intent filter testing

--- app/src/main/java/com/example/testapp/MainActivity.java
+++ app/src/main/java/com/example/testapp/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.testapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final static String TEXT = "PARAM";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("example://intent");
+                Intent runIntent = new Intent(Intent.ACTION_VIEW, uri);
+                runIntent.putExtra(TEXT, "test intent");
+                startActivity(runIntent);
+            }
+        });
     }
 }
